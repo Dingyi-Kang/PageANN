255009848abeb32b37e643465b4852a5cb405783
Dingyi Kang <dingyikangosu@gmail.com>
2025-10-23 10:24:52 -0500

    Fixed a bug. Recovered the PQ generation in vamana disk graph construction

--- src/disk_utils.cpp
+++ src/disk_utils.cpp
@@ -2455,8 +2455,8 @@ int build_disk_index(const char *dataFilePath, const char *indexFilePath, const 
     diskann::cout << "Compressing " << dim << "-dimensional data into " << num_pq_chunks << " bytes per vector."
                   << std::endl;
 
-    // generate_quantized_data<T>(data_file_to_use, pq_pivots_path, pq_compressed_vectors_path, compareMetric, p_val,
-    //                            num_pq_chunks, use_opq, codebook_prefix);
+    generate_quantized_data<T>(data_file_to_use, pq_pivots_path, pq_compressed_vectors_path, compareMetric, p_val,
+                               num_pq_chunks, use_opq, codebook_prefix);
     diskann::cout << timer.elapsed_seconds_for_step("generating quantized data") << std::endl;
 
 // Gopal. Splitting diskann_dll into separate DLLs for search and build.
